a5599382e2f610afbe792ccc3590bbb91efd660a
CodeWithRider <priyankapal8823@gmail.com>
2024-05-14 13:29:00 +0530

    my files

---
+++ Portfolio.js
@@ -0,0 +1,16 @@
+// var typed = new Typed("#element",{
+//     Strings:["Full stack Developer","Dancer" ],
+//     typeSpeed:100,
+//     backspeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
+var typed = new Typed('#element', {
+    strings: ["Full stack Developer","Dancer"],
+    typeSpeed: 100,
+    backspeed:100,
+    backDelay:1000,
+    loop:true
+  });
+
+
